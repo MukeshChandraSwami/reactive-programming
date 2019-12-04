6b48d2af85376a39fda1dda2dbfaa87a254b390e
Mukesh CHANDRA <G522141@mph.morpho.com>
2019-12-04 23:55:30 +0530

    Writting test cases for Flux and Mono

--- src/test/java/com/learn/reactive/fluxandmono/flux/FluxTest.java
+++ src/test/java/com/learn/reactive/fluxandmono/flux/FluxTest.java
@@ -1,7 +1,9 @@
 package com.learn.reactive.fluxandmono.flux;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.test.StepVerifierOptions;
 
 public class FluxTest {
 
@@ -10,7 +12,7 @@ public class FluxTest {
 
         Flux.just("Spring", "DS", "Reactive Programming", "National Holiday")
                 .concatWith(Flux.just("Before Error"))
-               // .concatWith(Flux.error(() -> new RuntimeException("There is an error")))
+                .concatWith(Flux.error(() -> new RuntimeException("There is an error")))
                 .concatWith(Flux.just("After Error"))
                 .log()
                 .subscribe(data -> System.out.println(data),
@@ -18,4 +20,43 @@ public class FluxTest {
                             System.out.println("Handling error : " + e.getMessage());
                 }, () -> System.out.println("Successfully Completed"));
     }
+
+    @Test
+    public void fluxTest_without_error(){
+
+        Flux<String> flux = Flux.just("Spring", "DS", "Reactive Programming", "National Holiday")
+                .log();
+        StepVerifier.create(flux, StepVerifierOptions.create())
+                .expectNext("Spring")
+                .expectNext("DS","Reactive Programming","National Holiday")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void fluxTest_with_error(){
+
+        Flux<String> flux = Flux.just("Spring", "DS", "Reactive Programming", "National Holiday")
+                .concatWith(Flux.error(() -> new NullPointerException("There is some issue")))
+                .log();
+
+        StepVerifier.create(flux, StepVerifierOptions.create())
+                .expectNext("Spring")
+                .expectNext("DS","Reactive Programming","National Holiday")
+                .verifyError();
+    }
+
+    @Test
+    public void fluxTest_with_error_publish_data_counter(){
+
+        Flux<String> flux = Flux.just("Spring", "DS", "Reactive")
+                .log()
+                .concatWith(Flux.error(new RuntimeException()));
+
+        StepVerifier.create(flux)
+                .expectNext("Spring")
+                .expectNextCount(2)
+                .verifyError(Exception.class);
+    }
+
 }
--- src/test/java/com/learn/reactive/fluxandmono/mono/MonoTest.java
+++ src/test/java/com/learn/reactive/fluxandmono/mono/MonoTest.java
@@ -1,4 +1,18 @@
 package com.learn.reactive.fluxandmono.mono;
 
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public class MonoTest {
+
+    @Test
+    public void monoTest(){
+
+        Mono<String> mono = Mono.just("Reactive Programming")
+                .log();
+
+        mono.subscribe(System.out::println);
+    }
+
 }
